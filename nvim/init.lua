74a720106821d6d3bb32f7e23569a4cee56483bd
Peter-Immanuel <peterbemshima@gmail.com>
2024-01-26 12:39:30 +0100

    First commit of termial setup and nvim setup

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+require("bemshima.plugins-setup")
+require("bemshima.core.options")
+require("bemshima.core.keymaps")
+require("bemshima.core.colorscheme")
+require("bemshima.plugins.comment")
+require("bemshima.plugins.nvim-tree")
+require("bemshima.plugins.lualine")
+require("bemshima.plugins.telescope")
+
